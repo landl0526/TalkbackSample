073c5b6c4322b8cb0cc1c13e78dc43d163ae540b
Land Lu <v-luting@microsoft.com>
2020-04-09 13:28:14 +0800

    Add project files.

---
+++ TalkBackSample/TalkBackSample.Android/CustomLabelRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TalkBackSample.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace TalkBackSample.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+
+        }
+    }
+}
